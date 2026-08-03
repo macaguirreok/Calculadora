16ebfe22e270fe717558a72d0a616493852b4163
macaguirreok <maquienyt@gmail.com>
2026-08-02 21:37:18 -0300

    cambios en estructura de la calculadora

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// LLama a los input
+
+const inputUno = document.getElementById("numero1");
+
+
+// Llama h3 resultado
+
+const rest = document.getElementById("Resultado");
+
+// llamo a los botones
+
+const btnSumar = document.getElementById("suma");
+
+const btnRestar = document.getElementById("resta");
+
+const btnDividir = document.getElementById("division");
+
+const btnMultiplicar = document.getElementById("multiplicacion");
+
+const btnIgual = document.getElementById("igual");
+
+//* Eventos de los botones
+
+// Sumar
+
